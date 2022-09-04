717013ec987669338a6a099a6cbf3be78468441c
Bradley Meyer <bradleymeyer@Bradleys-MBP.lan>
2022-09-04 13:45:13 -0400

    adding file structure

---
+++ server.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/404.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
